826f826df5589d9cf62c310615e19798cb66a039
Krystian Wieczorek <krystianwieczorek70@gmail.com>
2021-12-22 15:25:36 +0100

    commit

--- src/pl/akademia108/Main.java
+++ src/pl/akademia108/Main.java
@@ -1,8 +1,20 @@
 package pl.akademia108;
-
+import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int[] calkowite = new int[6];
+
+        for (short i = 0; i < 6 ; ++i) {
+            int min = 1;
+            int max = 49;
+            Random r = new Random();
+            calkowite[i] = r.nextInt((max-min) + 1) + min;
+        }
+
+        for (int liczba : calkowite) {
+            System.out.println(liczba);
+        }
+
     }
 }
